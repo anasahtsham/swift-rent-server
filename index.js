39300772849f41d0237ac2663ba22f356f460276
anasahtsham <mh.anasahtsham@gmail.com>
2023-12-02 17:08:41 +0500

    3rd API implemented: Register Account

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import pg from "pg";
+import pg from "pg"; //postgress database
+
+//Connecting Database
 const db = new pg.Client({
     user: "postgres",
     host: "localhost",
@@ -7,21 +9,28 @@ const db = new pg.Client({
     port: "5432",
 });
 db.connect();
-import express from "express";
+
+import express from "express"; //express server
 import bodyParser from "body-parser";
-import morgan from "morgan";
+import morgan from "morgan"; //logging tool
+
+//finding out directory folder
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+//Initiating Express Server
 const app = express();
 const port = 3000;
 
+//Parser
 app.use(bodyParser.urlencoded({ extended: true }));
+//Logger
 app.use(morgan("tiny"));
 
 // API 1: Login
 app.post('/api/login', async (req, res) => {
+    //Inputs
     const { emailOrPhone, password } = req.body;
     try {
       // Check if the email or number exists in the User table
@@ -54,7 +63,7 @@ app.post('/api/login', async (req, res) => {
       }
   
       // Return the user ID, userType, and success status
-      return res.json({ userID, ownerID, tenantID, success: true });
+      return res.status(200).json({ userID, ownerID, tenantID, success: true });
   
     } catch (error) {
       console.error("Error during login:", error);
@@ -64,6 +73,7 @@ app.post('/api/login', async (req, res) => {
 
 // API 2: Signup Contact Verification
 app.post('/api/signup-contact', async (req, res) => {
+    //Inputs
     const { email, phone } = req.body;
     try {
         let emailExists = 0, phoneExists = 0;
@@ -96,11 +106,35 @@ app.post('/api/signup-contact', async (req, res) => {
     }
 })
 
-// API 3:Signup Complete
+// API 3: Register Account
+app.post('/api/register-account', async (req,res) => {
+    //Inputs
+    const {userType, firstName, lastName, DOB, email, phone, password} = req.body;
+    try {
+        //Creating a new record in user table
+        const userQuery = await db.query('INSERT INTO UserInformation (firstname, lastname, dob, email, phone, md5password) VALUES ($1,$2,$3,$4,$5,$6) RETURNING id;', [firstName, lastName, DOB, email, phone, password]);
+        //Extracting userID
+        const userID = userQuery.rows[0].id;        
+        //Create the type of user (Owner or Tenant)
+        let ownerID = 0, tenantID = 0; 
+        if (userType === 'owner') {
+            const ownerQuery = await db.query('INSERT INTO Owner (userID) VALUES ($1) RETURNING id;', [userID]);
+            ownerID = ownerQuery.rows[0].id;
+        }else{
+            const tenantQuery = await db.query('INSERT INTO Tenant (userID) VALUES ($1) RETURNING id;', [userID]);
+            tenantID = tenantQuery.rows[0].id;
+        }
+        //Return userID, ownerID, tenantID, success status
+        return res.status(200).json({ userID, ownerID, tenantID, success: true});
+    } catch (error) {
+        console.error("Error during registration of user:", error);
+        return res.status(500).json({ error: "Internal Server Error"});
+    }
+});
 
-// API 4:
+// API 4: 
 
-// API 5:
+// API 5: 
 
 // API 6:
 
