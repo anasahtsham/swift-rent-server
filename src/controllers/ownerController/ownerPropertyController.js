4ff56e19a675584c8562cce5160a2b1400b077e7
Siamax <ibrahimahtsham2002@gmail.com>
2024-03-27 16:35:40 +0500

    Fix sorting order in fetchPropertyData function

--- src/controllers/ownerController/ownerPropertyController.js
+++ src/controllers/ownerController/ownerPropertyController.js
@@ -30,7 +30,7 @@ export const fetchPropertyData = async (req, res) => {
     // Fetch data from Area table and sort by areaName
     const areaQuery = `SELECT id, areaName, cityID
 FROM Area
-ORDER BY cityID ASC, areaName ASC;`;
+ORDER BY id ASC;`;
     const areasResult = await db.query(areaQuery);
 
     // Create an array of areas without the parent property
