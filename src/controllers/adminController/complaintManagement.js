c89df48f42adae512966b6d743296bd481d26211
anas <mh.anasahtsham@gmail.com>
2024-04-30 01:07:55 +0500

    Admin API: Complaint List Implemented

--- src/controllers/adminController/complaintManagement.js
+++ src/controllers/adminController/complaintManagement.js
@@ -1,3 +1,31 @@
 import db from "../../config/config.js";
 
 //API 1: Complaint List
+export const getAdminComplaints = async (req, res) => {
+  try {
+    // Query to retrieve admin complaints ordered by ID in descending order
+    const adminComplaintsQuery = `
+        SELECT 
+          AC.id,
+          AC.senderID,
+          CONCAT(UI.firstName, ' ', UI.lastName) AS senderName,
+          AC.senderType,
+          AC.complaintTitle,
+          AC.complaintDescription,
+          AC.complaintStatus,
+          TO_CHAR(AC.createdOn, 'DD-MM-YYYY') AS complaintDate,
+          TO_CHAR(AC.createdOn, 'HH24:MI:SS') AS complaintTime
+        FROM AdminComplaint AC
+        JOIN UserInformation UI ON AC.senderID = UI.id
+        ORDER BY AC.id DESC;
+      `;
+
+    const { rows } = await db.query(adminComplaintsQuery);
+
+    // Send admin complaints as JSON
+    res.status(200).json({ adminComplaints: rows });
+  } catch (error) {
+    console.error("Error retrieving admin complaints:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -7,7 +7,7 @@ import * as complaintManagement from "../controllers/adminController/complaintMa
 
 //Authentication
 adminRouter.post("/login", authentication.login);
-//Complaint Management
+//Area Management
 adminRouter.get("/city-list", areaManagement.cityList);
 adminRouter.post("/add-city", areaManagement.addCity);
 adminRouter.post("/delete-city", areaManagement.deleteCity);
@@ -16,7 +16,9 @@ adminRouter.post("/area-list", areaManagement.areaList);
 adminRouter.post("/add-area", areaManagement.addArea);
 adminRouter.post("/delete-area", areaManagement.deleteArea);
 adminRouter.post("/update-area", areaManagement.updateArea);
+//Compaint Management
+adminRouter.get("/complaint-list", complaintManagement.getAdminComplaints);
 
-//Total adminRouter routes: 9
+//Total adminRouter routes: 10
 
 export default adminRouter;
