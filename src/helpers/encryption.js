cc7325148f4f4895318af3e8abee50d9707e05f7
anas <mh.anasahtsham@gmail.com>
2024-05-04 00:28:57 +0500

    Refactor encryption.js to increase password hashing rounds based on SALT_ROUNDS environment variable

--- src/helpers/encryption.js
+++ src/helpers/encryption.js
@@ -3,9 +3,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+//Securing the password by adding salt and hashing it multiple times
+//This will make it difficult for the attacker to crack the password
+//The compute needed to crack the password will increase exponentially
+//But the cost for the server to authenticate the password will also increase
 export function hashPassword(password) {
   let hashedPassword = password + process.env.SALT;
-  for (let i = 0; i < 10; i++) {
+  let salt_rounds = parseInt(process.env.SALT_ROUNDS);
+  for (let i = 0; i < salt_rounds; i++) {
     hashedPassword = md5(hashedPassword);
   }
   return hashedPassword;
