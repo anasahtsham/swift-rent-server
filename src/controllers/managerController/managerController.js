a155b7b40bec59b5b2d5e7579d0110bb532208c7
anas <mh.anasahtsham@gmail.com>
2024-05-05 11:10:49 +0500

    Manager API: Display Hire Requests

--- src/controllers/managerController/managerController.js
+++ src/controllers/managerController/managerController.js
@@ -1,1 +1,52 @@
 import db from "../../config/config.js";
+
+//API 1: Get a list of all cities
+export const getCities = async (req, res) => {
+  try {
+    // Get the list of all cities
+    const query = `
+          SELECT cityName
+          FROM City
+          ORDER BY id ASC;
+        `;
+    const cities = await db.query(query);
+
+    // Send the list of cities as response
+    res.status(200).json(cities.rows);
+  } catch (error) {
+    console.error("Error fetching cities:", error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch cities.",
+    });
+  }
+};
+
+// API 2: Display all manager hire requests
+export const viewHireRequests = async (req, res) => {
+  try {
+    // Get the list of all pending hiring requests
+    const query = `
+        SELECT MHR.id AS "id (managerHireRequest)",
+               CONCAT(UI.firstName, ' ', UI.lastName) AS "owner name",
+               CONCAT(P.propertyAddress, ', ', A.areaName, ', ', C.cityName) AS "propertyAddress",
+               MHR.purpose
+        FROM ManagerHireRequest MHR
+        JOIN Property P ON MHR.propertyID = P.id
+        JOIN Area A ON P.areaID = A.id
+        JOIN City C ON A.cityID = C.id
+        JOIN UserInformation UI ON P.ownerID = UI.id
+        WHERE MHR.managerStatus = 'P'
+        ORDER BY MHR.id DESC;
+      `;
+    const hiringRequests = await db.query(query);
+
+    // Send the list of hiring requests as response
+    res.status(200).json(hiringRequests.rows);
+  } catch (error) {
+    console.error("Error viewing hiring requests:", error);
+    res
+      .status(500)
+      .json({ success: false, message: "Failed to view hiring requests." });
+  }
+};
--- src/routes/managerRoutes.js
+++ src/routes/managerRoutes.js
@@ -3,4 +3,7 @@ const managerRouter = express.Router();
 
 import * as managerController from "../controllers/managerController/managerController.js";
 
+managerRouter.get("/get-cities", managerController.getCities);
+managerRouter.get("/view-hire-requests", managerController.viewHireRequests);
+
 export default managerRouter;
